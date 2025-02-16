da5e3d6a5db9b4829d21ab268f9f186115e55dcb
Aditya Advani <aditya@bestparents.com>
2025-02-15 17:27:05 -0800

    missing file

--- holdem/jest.config.js
+++ holdem/jest.config.js
@@ -1,12 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   testMatch: [
     '**/*.test.ts'
   ],
-  verbose: true
+  verbose: true,
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
 }; 
